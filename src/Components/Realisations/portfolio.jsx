71d0aeb4ab6ec7106e68c36b1e91f9e87d1640e6
Yankhoba <bagayoko.yan@gmail.com>
2023-11-22 10:49:59 +0100

    🚧 mise en place de modal que je doit adapter à mon code

--- src/Components/Realisations/portfolio.jsx
+++ src/Components/Realisations/portfolio.jsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
 import './portfolio.css'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 function Realisation() {
+    const [modalShow, setModalShow] = useState(false);
   return (
     <>
       <section id="citation">
@@ -24,6 +56,16 @@ function Realisation() {
           <img src="https://placehold.co/100x100" alt="" className="realisationImg" />
         </div>
       </section>
+      <Button variant='primary'  onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+
+      
     </>
   );
 }
