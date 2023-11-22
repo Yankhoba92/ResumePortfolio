44da12819cbc746750d7955104894ba9dfd5f8db
Yankhoba <bagayoko.yan@gmail.com>
2023-11-22 11:21:25 +0100

    💄 stylisation des images

--- src/Components/Realisations/portfolio.jsx
+++ src/Components/Realisations/portfolio.jsx
@@ -1,71 +1,64 @@
-import { useState } from 'react';
-import './portfolio.css'
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
+import "./portfolio.css";
 
-function MyVerticallyCenteredModal(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+
 function Realisation() {
-    const [modalShow, setModalShow] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
   return (
     <>
       <section id="citation">
-        <p className="citationDescription">
-          La vraie sagesse réside dans la mise en pratique. C'est là que le
-          savoir devient expérience, et l'expérience, une leçon qui guide notre
-          chemin.
-        </p>
+        <div className="description">
+          <p className="citationDescription">
+            La vraie sagesse réside dans la mise en pratique. <br />
+            C'est là que le savoir devient expérience,
+            <br /> et l'expérience,une leçon qui guide notre chemin.
+          </p>
+        </div>
       </section>
-      <section id="realisation">
-        <p className="realisationText">
+      <section id="realisations">
+        <h2 className="realisationsTitle">My Portfolio</h2>
+        <p className="realisationsDescription">
           La vraie sagesse réside dans la mise en pratique. C'est là que le
           savoir devient expérience, et l'expérience, une leçon qui guide notre
           chemin.
         </p>
         <div className="realisationsImgs">
-          <img src="https://placehold.co/100x100" alt="" className="realisationImg" />
-          <img src="https://placehold.co/100x100" alt="" className="realisationImg" />
-          <img src="https://placehold.co/100x100" alt="" className="realisationImg" />
-          <img src="https://placehold.co/100x100" alt="" className="realisationImg" />
-          <img src="https://placehold.co/100x100" alt="" className="realisationImg" />
-          <img src="https://placehold.co/100x100" alt="" className="realisationImg" />
+            
+          <img
+            src="https://placehold.co/100x100"
+            alt=""
+            className="realisationImg"
+          />
+          <img
+            src="https://placehold.co/100x100"
+            alt=""
+            className="realisationImg"
+          />
+          <img
+            src="https://placehold.co/100x100"
+            alt=""
+            className="realisationImg"
+          />
+          <img
+            src="https://placehold.co/100x100"
+            alt=""
+            className="realisationImg"
+          />
+          <img
+            src="https://placehold.co/100x100"
+            alt=""
+            className="realisationImg"
+          />
+          <img
+            src="https://placehold.co/100x100"
+            alt=""
+            className="realisationImg"
+          />
         </div>
+        <button className="realisationBtn">
+    Voir Plus
+        </button>
       </section>
-      <Button variant='primary'  onClick={() => setModalShow(true)}>
-        Launch vertically centered modal
-      </Button>
-
-      <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
-
-      
     </>
   );
 }
